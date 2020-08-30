992e9290e03734bf1ce5ddf99ae2bdc025dc6848
mlite <mlite@users.noreply.github.com>
2020-08-29 18:15:35 -0700

    replace tab with spaces

--- musl-lsearch.c
+++ musl-lsearch.c
@@ -5,29 +5,29 @@
 #endif
 
 void *musl_lsearch(const void *key, void *base, size_t *nelp, size_t width,
-	int (*compar)(const void *, const void *))
+  int (*compar)(const void *, const void *))
 {
-	char **p = (char **)base;
-	size_t n = *nelp;
-	size_t i;
+  char **p = (char **)base;
+  size_t n = *nelp;
+  size_t i;
 
-	for (i = 0; i < n; i++)
-		if (compar(p[i], key) == 0)
-			return p[i];
-	*nelp = n+1;
-	// b.o. here when width is longer than the size of key
-	return memcpy(p[n], key, width);
+  for (i = 0; i < n; i++)
+    if (compar(p[i], key) == 0)
+      return p[i];
+  *nelp = n+1;
+  // b.o. here when width is longer than the size of key
+  return memcpy(p[n], key, width);
 }
 
 void *musl_lfind(const void *key, const void *base, size_t *nelp,
-	size_t width, int (*compar)(const void *, const void *))
+  size_t width, int (*compar)(const void *, const void *))
 {
-	char **p = (char **)base;
-	size_t n = *nelp;
-	size_t i;
+  char **p = (char **)base;
+  size_t n = *nelp;
+  size_t i;
 
-	for (i = 0; i < n; i++)
-		if (compar(p[i], key) == 0)
-			return p[i];
-	return 0;
+  for (i = 0; i < n; i++)
+    if (compar(p[i], key) == 0)
+      return p[i];
+  return 0;
 }
